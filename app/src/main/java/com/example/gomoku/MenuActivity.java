6f55a794d6097edbf7cccf7ec178882e5995921c
Tin <mind.hacker195@gmail.com>
2019-05-21 15:42:19 +0200

    menu

---
+++ app/src/main/java/com/example/gomoku/MenuActivity.java
@@ -0,0 +1,52 @@
+package com.example.gomoku;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button startpvp,settings,startpve;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+        setListen();
+    }
+    private void setListen() {
+        startpvp = (Button) findViewById(R.id.startpvp);
+        settings = (Button) findViewById(R.id.settings);
+        startpve = (Button) findViewById(R.id.startpve);
+
+        startpvp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startpvp();
+            }
+        });
+        settings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                settings();
+            }
+        });
+        startpve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startpve();
+            }
+        });
+    }
+
+    private void startpvp() {
+        Intent i = new Intent(MenuActivity.this, MainActivity.class);
+        startActivity(i);
+    }
+    private void settings() {
+
+    }
+    private void startpve() {
+
+    }
+}
